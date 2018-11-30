8584d7c823ce4d1f66fcd09c13296e5abf618a30
Tony Galmiche <tony.galmiche@infosaone.com>
2018-11-30 13:03:38 +0100

    Mise en ligne

---
+++ static/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "InfoSaône - Module Odoo pour Plastigray en 2019",
+    "version" : "0.1",
+    "author" : "InfoSaône",
+    "category" : "Plastigray",
+    "description": """
+InfoSaône - Module Odoo pour Plastigray en 2019
+===================================================
+InfoSaône - Module Odoo pour Plastigray en 2019
+    """,
+    "maintainer": 'InfoSaône',
+    "website": 'http://www.infosaone.com',
+    "depends" : [
+        "is_plastigray",
+    ], 
+    "data" : [
+    ], 
+    "qweb": [
+    ],
+    "installable": True,
+    "active": False
+}
+
