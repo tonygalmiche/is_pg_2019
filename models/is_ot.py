357fe01647db19f264c9ef74fcb95f0c60008491
Tony Galmiche <tony.galmiche@infosaone.com>
2019-07-14 11:31:51 +0200

    add is_site_ids

--- models/is_ot.py
+++ models/is_ot.py
@@ -199,7 +199,7 @@ class is_ot(models.Model):
             ('annule', u'Annulé'),
             ('termine', u'Terminé'),
             ], "State", readonly=True, default="creation")
-    site_id             = fields.Many2one("is.database", "Site", help="Site must be filled in its user form")
+    site_id             = fields.Many2one("is.database", "Site", required=True)
     emplacement         = fields.Char("Emplacement", store=True, readonly=True, compute='_compute_emplacement')
     date_creation       = fields.Date(u"Date de création", copy=False, default=fields.Date.context_today, readonly=True)
     demandeur_id        = fields.Many2one("res.users", "Demandeur", default=lambda self: self.env.uid, readonly=True)
