f83e6814c8ded74d6fa394e7f3dede010adb4d02
Tony Galmiche <tony.galmiche@infosaone.com>
2020-01-29 13:06:09 +0100

    modifs suite tests cc

--- models/is_gestion_des_absences.py
+++ models/is_gestion_des_absences.py
@@ -15,6 +15,7 @@ class hr_employee(models.Model):
 class is_demande_conges(models.Model):
     _name        = 'is.demande.conges'
     _description = u'Demande de congés'
+    _order       = 'name desc'
 
     @api.multi
     def envoi_mail(self, email_from, email_to, email_cc, subject, body_html):
@@ -31,20 +32,20 @@ class is_demande_conges(models.Model):
                 self.env['mail.mail'].send(email)
 
     @api.multi
-    def niveau1_send_mail_action(self):
+    def vers_validation_n1_action(self):
         for obj in self:
             obj.date_validation_n1 = datetime.datetime.today()
-            subject = u'[' + obj.name + u'] Sent Mail to - Valideur Niveau 1 '
+            subject = u'[' + obj.name + u'] Demande de congés - Validation Niveau 1 '
             email_to = obj.valideur_n1.email
             user = self.env['res.users'].browse(self._uid)
             email_from = user.email
             email_cc = obj.createur_id.email
             nom = user.name
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.ot'
+            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.demande.conges'
             body_html = u""" 
                 <p>Bonjour,</p> 
-                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + obj.name + """</a> à l'état 'Niveau 1'.</p> 
+                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + obj.name + """</a> à l'état 'Validation Niveau 1'.</p> 
                 <p>Merci d'en prendre connaissance.</p> 
             """ 
             self.envoi_mail(email_from, email_to, email_cc, subject, body_html)
@@ -52,20 +53,20 @@ class is_demande_conges(models.Model):
         return True
 
     @api.multi
-    def niveau2_send_mail_action(self):
+    def vers_validation_n2_action(self):
         for obj in self:
             obj.date_validation_n2 = datetime.datetime.today()
-            subject = u'[' + obj.name + u'] Sent Mail to - Vers Validation Niveau 2'
+            subject = u'[' + obj.name + u'] Demande de congés - Validation Niveau 2'
             email_to = obj.valideur_n2.email
             user = self.env['res.users'].browse(self._uid)
             email_from = user.email
             email_cc = obj.createur_id.email
             nom = user.name
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.ot'
+            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.demande.conges'
             body_html = u""" 
                 <p>Bonjour,</p> 
-                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + obj.name + """</a> à l'état 'Niveau 2'.</p> 
+                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + obj.name + """</a> à l'état 'Validation Niveau 2'.</p> 
                 <p>Merci d'en prendre connaissance.</p> 
             """ 
             self.envoi_mail(email_from, email_to, email_cc, subject, body_html)
@@ -73,17 +74,17 @@ class is_demande_conges(models.Model):
         return True
 
     @api.multi
-    def rh_send_mail_action(self):
+    def vers_validation_rh_action(self):
         for obj in self:
             obj.date_validation_rh = datetime.datetime.today()
-            subject = u'[' + obj.name + u'] Sent Mail to - Validation RH'
+            subject = u'[' + obj.name + u'] Demande de congés - Validation RH'
             email_to = obj.responsable_rh_id.email
             user = self.env['res.users'].browse(self._uid)
             email_from = user.email
             email_cc = obj.createur_id.email
             nom = user.name
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.ot'
+            url = base_url + u'/web#id=' + str(obj.id) + u'&view_type=form&model=is.demande.conges'
             body_html = u""" 
                 <p>Bonjour,</p> 
                 <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + obj.name + """</a> à l'état 'Validation RH'.</p> 
@@ -99,11 +100,6 @@ class is_demande_conges(models.Model):
             obj.signal_workflow('solde')
         return True
 
-    @api.multi
-    def vers_annule_action(self):
-        for obj in self:
-            obj.signal_workflow('annule')
-        return True
 
     @api.model
     def default_get(self, default_fields):
@@ -119,61 +115,76 @@ class is_demande_conges(models.Model):
         vals['name'] = self.env['ir.sequence'].get('is.demande.conges') or ''
         return super(is_demande_conges, self).create(vals)
 
-    @api.depends('date_debut', 'demandeur_id')
-    def _get_visible(self):
+
+    @api.depends('state','demandeur_id','createur_id','responsable_rh_id','valideur_n1','valideur_n1')
+    def _get_btn_vsb(self):
+        uid = self._uid
         for obj in self:
+            vers_creation       = False
+            vers_annuler        = False
+            vers_validation_n1  = False
+            vers_validation_n2  = False
+            vers_validation_rh  = False
+            vers_solde          = False
+            fld_vsb             = False
+            test_date           = False
             if obj.date_debut:
                 current_date = datetime.date.today()
                 current_date_plus_ten = current_date + relativedelta(days=10)
-                if obj.state == 'validation_n1':
-                    if str(current_date_plus_ten) < obj.date_debut:
-                        if obj.createur_id and obj.createur_id.id == self._uid or obj.demandeur_id and obj.demandeur_id.id == self._uid:
-                            obj.vers_creation_annuler_btn_vsb = True
-                    if str(current_date) <= obj.date_debut:
-                        if obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                            obj.vers_creation_annuler_btn_vsb = True
-                if obj.state == 'validation_n2':
-                    if str(current_date_plus_ten) < obj.date_debut:
-                        if obj.valideur_n2 and obj.valideur_n2.id == self._uid or obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                            obj.vers_creation_annuler_btn_vsb = True
-                    if str(current_date) <= obj.date_debut:
-                        if obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                            obj.vers_creation_annuler_btn_vsb = True
-# 
-    @api.depends('state','demandeur_id','createur_id','responsable_rh_id','valideur_n1','valideur_n1')
-    def _get_btn_visible(self):
-        for obj in self:
-            if obj.state == 'validation_n1' and obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid \
-                                            or obj.valideur_n2 and obj.valideur_n2.id == self._uid:
-                obj.valid_btn_vsb = True
-            if obj.state == 'validation_rh' and obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                obj.valid_btn_vsb = True
-            if obj.state in ['validation_n2','validation_n1','creation'] and obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                obj.valid_btn_vsb = True
-            if self._uid == 1:
-                obj.valid_btn_vsb = True
-            if obj.responsable_rh_id and obj.responsable_rh_id.id == self._uid:
-                obj.fld_vsb = True
+                if str(current_date_plus_ten) < obj.date_debut:
+                    test_date = True
+
+            if test_date:
+                if obj.state == 'validation_n1' or obj.state == 'validation_n2' or obj.state == 'validation_rh':
+                    if obj.createur_id.id == uid or obj.demandeur_id.id == uid or obj.valideur_n1.id == uid or obj.valideur_n2.id == uid or obj.responsable_rh_id.id == uid:
+                        vers_creation = True
+
+            if obj.state == 'validation_rh' and  obj.responsable_rh_id.id == uid:
+                fld_vsb = True
+            if obj.state == 'creation':
+                if obj.createur_id.id == uid or obj.demandeur_id.id == uid or obj.valideur_n1.id == uid or obj.valideur_n2.id == uid or obj.responsable_rh_id.id == uid:
+                    vers_validation_n1 = True
+            if obj.state == 'validation_n1':
+                if obj.valideur_n1.id == uid or obj.valideur_n2.id == uid or obj.responsable_rh_id.id == uid:
+                    vers_validation_n2 = True
+                    vers_annuler       = True
+            if obj.state == 'validation_n2':
+                if obj.valideur_n2.id == uid or obj.responsable_rh_id.id == uid:
+                    vers_validation_rh = True
+                    vers_annuler       = True
+            if obj.state == 'validation_rh':
+                if obj.responsable_rh_id.id == uid:
+                    vers_solde   = True
+                    vers_annuler = True
+
+
+            obj.vers_creation_btn_vsb      = vers_creation
+            obj.vers_annuler_btn_vsb       = vers_annuler
+            obj.vers_validation_n1_btn_vsb = vers_validation_n1
+            obj.vers_validation_n2_btn_vsb = vers_validation_n2
+            obj.vers_validation_rh_btn_vsb = vers_validation_rh
+            obj.vers_solde_btn_vsb         = vers_solde
+            obj.fld_vsb                    = fld_vsb
 
 
     name                          = fields.Char(u"N° demande")
-    createur_id                   = fields.Many2one('res.users', u'Créateur', default=lambda self: self.env.user)
-    date_creation                 = fields.Datetime(string=u'Date de création', default=lambda *a: fields.datetime.now())
+    createur_id                   = fields.Many2one('res.users', u'Créateur', default=lambda self: self.env.user        , copy=False)
+    date_creation                 = fields.Datetime(string=u'Date de création', default=lambda *a: fields.datetime.now(), copy=False)
     demandeur_id                  = fields.Many2one('res.users', 'Demandeur', default=lambda self: self.env.user)
     valideur_n1                   = fields.Many2one('res.users', 'Valideur Niveau 1')
-    date_validation_n1            = fields.Datetime(string='Date validation N1')
+    date_validation_n1            = fields.Datetime(string='Date validation N1', copy=False)
     valideur_n2                   = fields.Many2one('res.users', 'Valideur Niveau 2')
-    date_validation_n2            = fields.Datetime(string='Date validation N2')
+    date_validation_n2            = fields.Datetime(string='Date validation N2', copy=False)
     responsable_rh_id             = fields.Many2one('res.users', 'Responsable RH', default=lambda self: self.env.user.company_id.is_responsable_rh_id)
-    date_validation_rh            = fields.Datetime(string='Date Responsable RH')
+    date_validation_rh            = fields.Datetime(string='Date Responsable RH', copy=False)
     type_demande                  = fields.Selection([
                                         ('cp_rtt_journee', u'CP ou RTT par journée entière'),
                                         ('cp_rtt_demi_journee', u'CP ou RTT par ½ journée'),
                                         ('rc_heures', 'RC en heures'),
                                         ], string='Type de demande')
-    cp                            = fields.Float(string='CP (jours)')
-    rtt                           = fields.Float(string='RTT (jours)')
-    rc                            = fields.Float(string='RC (jours)')
+    cp                            = fields.Float(string='CP (jours)' , copy=False)
+    rtt                           = fields.Float(string='RTT (jours)', copy=False)
+    rc                            = fields.Float(string='RC (heures)', copy=False)
     date_debut                    = fields.Date(string=u'Date début')
     date_fin                      = fields.Date(string='Date fin')
     le                            = fields.Date(string='Le')
@@ -183,18 +194,28 @@ class is_demande_conges(models.Model):
                                         ], string=u'Matin ou après-midi')
     heure_debut                   = fields.Integer(string=u'Heure début')
     heure_fin                     = fields.Integer(string=u'Heure fin')
-    raison_du_retour              = fields.Text(string='Raison du retour')
-    raison_annulation             = fields.Text(string='Raison annulation')
+    raison_du_retour              = fields.Text(string='Motif du retour', copy=False)
+    raison_annulation             = fields.Text(string='Motif du refus' , copy=False)
     state                         = fields.Selection([
                                         ('creation', u'Création'),
                                         ('validation_n1', 'Validation niveau 1'),
                                         ('validation_n2', 'Validation niveau 2'),
                                         ('validation_rh', 'Validation RH'),
                                         ('solde', 'Soldé'),
-                                        ('annule', u'Annulé')], string='State', readonly=True, default='creation')
-    vers_creation_annuler_btn_vsb = fields.Boolean(string='Creation Annuler Btn Vsb', compute='_get_visible', default=False, readonly=True)
-    valid_btn_vsb                 = fields.Boolean(string='Valide Btn Vsb', compute='_get_btn_visible', default=False, readonly=True)
-    fld_vsb                       = fields.Boolean(string='Field Vsb', compute='_get_btn_visible', default=False, readonly=True)
+                                        ('annule', u'Refusé')], string='State', readonly=True, default='creation')
+
+    vers_creation_btn_vsb      = fields.Boolean(string='vers_creation_btn_vsb'     , compute='_get_btn_vsb', default=False, readonly=True)
+    vers_annuler_btn_vsb       = fields.Boolean(string='vers_annuler_btn_vsb'      , compute='_get_btn_vsb', default=False, readonly=True)
+    vers_validation_n1_btn_vsb = fields.Boolean(string='vers_validation_n1_btn_vsb', compute='_get_btn_vsb', default=False, readonly=True)
+    vers_validation_n2_btn_vsb = fields.Boolean(string='vers_validation_n2_btn_vsb', compute='_get_btn_vsb', default=False, readonly=True)
+    vers_validation_rh_btn_vsb = fields.Boolean(string='vers_validation_rh_btn_vsb', compute='_get_btn_vsb', default=False, readonly=True)
+    vers_solde_btn_vsb         = fields.Boolean(string='vers_solde_btn_vsb'        , compute='_get_btn_vsb', default=False, readonly=True)
+    fld_vsb                    = fields.Boolean(string='Field Vsb'                 , compute='_get_btn_vsb', default=False, readonly=True)
+
+
+    #vers_creation_annuler_btn_vsb = fields.Boolean(string='Creation Annuler Btn Vsb', compute='_get_visible', default=False, readonly=True)
+    #valid_btn_vsb                 = fields.Boolean(string='Valide Btn Vsb', compute='_get_btn_visible', default=False, readonly=True)
+
 
 
 class is_demande_absence_type(models.Model):
--- wizard/is_gestion_des_absences_wiz.py
+++ wizard/is_gestion_des_absences_wiz.py
@@ -6,24 +6,24 @@ from openerp import models, fields, api
 class is_gestion_des_absences_wiz(models.TransientModel):
     _name = 'is.gestion.des.absences.wiz'
 
-    conges_reason = fields.Text('Reason', required=True)
+    conges_reason = fields.Text('Motif', required=True)
 
     @api.multi
     def valider_reponse(self):
         conges_obj = self.env['is.demande.conges']
         if self._context and self._context.get('active_id'):
             conges = conges_obj.browse(self._context.get(('active_id')))
-            subject = u'[' + conges.name + u'] Sent Mail to createur_id - raison_du_retour'
-            email_to = conges.createur_id.id
+            subject = u'[' + conges.name + u'] Demande de congés - retour Création'
+            email_to = conges.createur_id.email
             user = self.env['res.users'].browse(self._uid)
             email_from = user.email
             email_cc = email_from
             nom = user.name
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            url = base_url + u'/web#id=' + str(conges.id) + u'&view_type=form&model=is.ot'
+            url = base_url + u'/web#id=' + str(conges.id) + u'&view_type=form&model=is.demande.conges'
             body_html = u""" 
                 <p>Bonjour,</p> 
-                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + conges.name + """</a> à l'état 'createur_id - raison_du_retour'.</p> 
+                <p>""" + nom + """ vient de repasser la Demande de congés <a href='""" + url + """'>""" + conges.name + """</a> à l'état 'Création'.</p> 
                 <p>Merci d'en prendre connaissance.</p> 
             """ 
             conges.sudo().raison_du_retour = self.conges_reason
@@ -37,24 +37,24 @@ class is_gestion_des_absences_wiz(models.TransientModel):
 class is_gestion_vers_annuler_wiz(models.TransientModel):
     _name = 'is.gestion.vers.annuler.wiz'
 
-    conges_reason = fields.Text('Reason', required=True)
+    conges_reason = fields.Text('Motif du refus', required=True)
 
     @api.multi
     def valider_reponse(self):
         conges_obj = self.env['is.demande.conges']
         if self._context and self._context.get('active_id'):
             conges = conges_obj.browse(self._context.get(('active_id')))
-            subject = u'[' + conges.name + u'] Sent Mail to createur_id - raison_annulation'
-            email_to = conges.createur_id.id
+            subject = u'[' + conges.name + u'] Demande de congés - Refus'
+            email_to = conges.createur_id.email
             user = self.env['res.users'].browse(self._uid)
             email_from = user.email
             email_cc = email_from
             nom = user.name
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-            url = base_url + u'/web#id=' + str(conges.id) + u'&view_type=form&model=is.ot'
+            url = base_url + u'/web#id=' + str(conges.id) + u'&view_type=form&model=is.demande.conges'
             body_html = u""" 
                 <p>Bonjour,</p> 
-                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + conges.name + """</a> à l'état 'createur_id - raison_annulation'.</p> 
+                <p>""" + nom + """ vient de passer la Demande de congés <a href='""" + url + """'>""" + conges.name + """</a> à l'état 'Refusé'.</p> 
                 <p>Merci d'en prendre connaissance.</p> 
             """ 
             conges.sudo().raison_annulation = self.conges_reason
