297ed51d158bf3ea96b36e66475fdd3f9ef4d38c
Tony Galmiche <tony.galmiche@infosaone.com>
2021-05-19 15:44:32 +0200

    add is_facture_proforma

--- models/is_facture_proforma.py
+++ models/is_facture_proforma.py
@@ -5,34 +5,122 @@ from openerp.exceptions import Warning
 import datetime
 
 
+class is_bon_transfert(models.Model):
+    _inherit = "is.bon.transfert"
+
+    @api.multi
+    def creation_facture_proforma_action(self):
+        for obj in self:
+            vals={
+                'adresse_liv_id'  : obj.partner_id.id,
+                'bon_transfert_id': obj.id,
+            }
+            proforma=self.env['is.facture.proforma'].create(vals)
+            poids_net = poids_brut = 0
+            sequence=0
+            for line in obj.line_ids:
+                sequence += 10
+                vals={
+                    'proforma_id': proforma.id,
+                    'sequence'   : sequence,
+                    'product_id' : line.product_id.id,
+                    'designation': line.product_id.name,
+                    'uom_id'     : line.uom_id.id,
+                    'quantite'   : line.quantite,
+                }
+                self.env['is.facture.proforma.line'].create(vals)
+            return {
+                'name': "Facture proforma",
+                'view_mode': 'form',
+                'view_type': 'form',
+                'res_model': 'is.facture.proforma',
+                'type': 'ir.actions.act_window',
+                'res_id': proforma.id,
+                'domain': '[]',
+            }
+
+
+class is_bl_manuel(models.Model):
+    _inherit = "is.bl.manuel"
+
+    @api.multi
+    def creation_facture_proforma_action(self):
+        for obj in self:
+            vals={
+                'adresse_liv_id': obj.destinataire_id.id,
+                'bl_manuel_id'  : obj.id,
+                'packaging'     : obj.colisage,
+                'informations'  : obj.motif_expedition,
+            }
+            proforma=self.env['is.facture.proforma'].create(vals)
+            poids_net = poids_brut = 0
+            for line in obj.line_ids:
+                poids_net  += line.poids_net
+                poids_brut += line.poids_brut
+                vals={
+                    'proforma_id': proforma.id,
+                    'sequence': line.sequence,
+                    'product_id': line.product_id.id,
+                    'designation': line.description,
+                    'uom_id': line.uom_id.id,
+                    'quantite': line.qt_livree,
+                }
+                self.env['is.facture.proforma.line'].create(vals)
+            proforma.poids_net  = poids_net
+            proforma.poids_brut = poids_brut
+            return {
+                'name': "Facture proforma",
+                'view_mode': 'form',
+                'view_type': 'form',
+                'res_model': 'is.facture.proforma',
+                'type': 'ir.actions.act_window',
+                'res_id': proforma.id,
+                'domain': '[]',
+            }
+
+
 class is_facture_proforma(models.Model):
     _name='is.facture.proforma'
     _order='name desc'
 
-    name                 = fields.Char("Facture proforma", readonly=True)
-#    createur_id          = fields.Many2one('res.users', 'Demandeur', required=True)
-#    date_demande         = fields.Date("Date de la demande", required=True        , default=lambda *a: fields.datetime.now())
-#    responsable_id       = fields.Many2one('res.users', 'Responsable', required=True, domain=lambda self: [( "groups_id", "=", self.env.ref("is_pg_2019.is_bon_achat_ville_grp").id)])
-#    fournisseur_id       = fields.Many2one('res.partner', 'Fournisseur', domain=[('is_achat_ville','=',True)], required=True)
-#    pricelist_id         = fields.Many2one('product.pricelist', "Liste de prix", related='fournisseur_id.property_product_pricelist_purchase', readonly=True)
-#    objet                = fields.Char("Objet de la demande", required=True)
-#    state                = fields.Selection([
-#        ('brouillon', 'Brouillon'),
-#        ('en_cours' , 'En cours de validation'),
-#        ('valide'   , 'Validé'),
-#        ('annule'   , 'Annulé'),
-#    ], "Etat", default='brouillon')
-    line_ids           = fields.One2many('is.facture.proforma.line', 'proforma_id', u"Lignes", copy=True)
-#    montant_total      = fields.Float("Montant Total", compute='_compute', readonly=True, store=True)
-#    nb_lignes          = fields.Integer("Nombre de lignes", compute='_compute', readonly=True, store=True)
-#    vers_brouillon_vsb = fields.Boolean('Champ technique', compute='_compute', readonly=True, store=False)
-#    vers_en_cours_vsb  = fields.Boolean('Champ technique', compute='_compute', readonly=True, store=False)
-#    vers_valide_vsb    = fields.Boolean('Champ technique', compute='_compute', readonly=True, store=False)
-#    vers_annule_vsb    = fields.Boolean('Champ technique', compute='_compute', readonly=True, store=False)
 
-#    _defaults = {
-#        'createur_id': lambda self, cr, uid, c: uid,
-#    }
+    @api.depends('adresse_liv_id')
+    def _compute_adresse_fac_id(self):
+        for obj in self:
+            obj.adresse_fac_id = obj.adresse_liv_id.is_adr_facturation.id or obj.adresse_liv_id.id
+
+
+    @api.depends('line_ids')
+    def _compute(self):
+        for obj in self:
+            montant = 0
+            for line in obj.line_ids:
+                montant += line.montant
+            obj.montant = montant
+
+
+    name                 = fields.Char("Facture proforma", readonly=True)
+    createur_id          = fields.Many2one('res.users', 'Créateur', required=True)
+    date_facture         = fields.Date("Date facture"             , required=True, default=lambda *a: fields.datetime.now())
+    adresse_liv_id       = fields.Many2one('res.partner', 'Adresse de livraison'  , required=True, domain=[('is_company','=',True),('customer','=',True)])
+    adresse_fac_id       = fields.Many2one('res.partner', 'Adresse de facturation', compute='_compute_adresse_fac_id', readonly=True, store=True)
+    pricelist_id         = fields.Many2one('product.pricelist', "Liste de prix"   , related='adresse_fac_id.property_product_pricelist', readonly=True)
+    vat                  = fields.Char("Numéro fiscal"                            , related='adresse_fac_id.vat', readonly=True)
+    incoterm_id          = fields.Many2one('stock.incoterms', "Incoterm"          , related='adresse_fac_id.is_incoterm', readonly=True)
+    lieu                 = fields.Char("Lieu"                                     , related='adresse_fac_id.is_lieu', readonly=True)
+    line_ids             = fields.One2many('is.facture.proforma.line', 'proforma_id', u"Lignes", copy=True)
+    poids_brut           = fields.Float("Poids brut")
+    poids_net            = fields.Float("Poids net")
+    packaging            = fields.Char("Packaging")
+    informations         = fields.Text("informations complémentaires")
+    bon_transfert_id     = fields.Many2one('is.bon.transfert', 'Bon de transfert', readonly=True)
+    bl_manuel_id         = fields.Many2one('is.bl.manuel', 'BL manuel', readonly=True)
+    montant              = fields.Float("Montant Total", digits=(14,2), compute='_compute', readonly=True, store=True)
+
+
+    _defaults = {
+        'createur_id': lambda self, cr, uid, c: uid,
+    }
 
     
     @api.model
@@ -54,19 +142,32 @@ class is_facture_proforma_line(models.Model):
     @api.depends('product_id','quantite','prix')
     def _compute(self):
         for obj in self:
-            #if obj.product_id:
-            #    obj.uom_id=obj.product_id.uom_po_id
             obj.montant=obj.quantite*obj.prix
 
-    proforma_id      = fields.Many2one('is.facture.proforma', "Facture proforma", required=True, ondelete='cascade', readonly=True)
-    sequence    = fields.Integer('Ordre')
-    product_id  = fields.Many2one('product.product', 'Article', domain=[('segment_id.name','=','ARTICLES COMPTABLES FRAIS GENERAUX')], required=True)
-    designation = fields.Char(u'Désignation', required=True)
-#    uom_id      = fields.Many2one('product.uom', "Unité d'achat", compute='_compute', readonly=True, store=True)
-    quantite    = fields.Float("Quantité", digits=(14,4), required=True)
-    prix        = fields.Float("Prix"    , digits=(14,4))
-    montant     = fields.Float("Montant", compute='_compute', readonly=True, store=True)
-#    chantier    = fields.Char(u'Chantier', help=u"Chantier sur 5 chiffres")
+
+    @api.onchange('product_id','quantite')
+    def _onchange_product_id(self):
+        self.nomenclature_douaniere = self.product_id.is_nomenclature_douaniere
+        self.designation            = self.product_id.name
+        prix = 0
+        product_id   = self.product_id.id
+        if product_id:
+            pricelist_id = self.proforma_id.pricelist_id.id
+            partner_id   = self.proforma_id.adresse_liv_id.id
+            price = self.proforma_id.pricelist_id.price_get(product_id, self.quantite, partner_id)
+            prix = price[pricelist_id]
+        self.prix = prix
+
+    proforma_id            = fields.Many2one('is.facture.proforma', "Facture proforma", required=True, ondelete='cascade', readonly=True)
+    sequence               = fields.Integer('Ordre')
+    product_id             = fields.Many2one('product.product', 'Article', domain=[('segment_id.name','=','ARTICLES COMPTABLES FRAIS GENERAUX')])
+    designation            = fields.Char(u'Désignation', required=True)
+    ref_client             = fields.Char(u'Référence client', related='product_id.is_ref_client', readonly=True)
+    nomenclature_douaniere = fields.Char(u'Nomenclature douanière')
+    uom_id                 = fields.Many2one('product.uom', "Unité", related='product_id.uom_id', readonly=True)
+    quantite               = fields.Float("Quantité", digits=(14,2), required=True)
+    prix                   = fields.Float("Prix"    , digits=(14,4))
+    montant                = fields.Float("Montant" , digits=(14,2), compute='_compute', readonly=True, store=True)
 
 
 
